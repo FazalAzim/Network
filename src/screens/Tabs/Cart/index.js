a350336bc91106605d065c1c227176994210c049
kamrankhan8 <xeroseven@gmail.com>
2024-02-01 16:48:15 +0500

    feat: cart screen created

---
+++ src/screens/Tabs/Cart/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './styles'
+export const Cart = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}

c73372f00736f28de328ae87dcbff3b844ad16a1
kamrankhan8 <xeroseven@gmail.com>
2024-02-01 16:50:07 +0500

    feat: inbox screen created

---
+++ src/screens/Tabs/Inbox/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './styles'
+
+export const Inbox = () => {
+  return (
+    <View>
+      <Text>Inbox</Text>
+    </View>
+  )
+}

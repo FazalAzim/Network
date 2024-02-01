6dca202dac3e03f1fd8b28cbf6a24da1d0927e94
kamrankhan8 <xeroseven@gmail.com>
2024-02-01 16:39:33 +0500

    feat: icons added

---
+++ src/assets/svg/index.js
@@ -0,0 +1,18 @@
+import Apple from './apple.svg';
+import Google from './google.svg';
+import Facebook from './fb.svg';
+import Chat from './chat.svg';
+import LogoText from './logo_text.svg';
+import Logo from './logo.svg';
+import AddressBook from './address_book.svg';
+import Favorites from './favorites.svg';
+import Product from './product.svg';
+import Schedules from './schedules.svg';
+import Settings from './settings.svg';
+import Star from './star.svg';
+import VideoLarge from './vid_icon_larg.svg';
+import VideoIcon from './vid_icon_small.svg';
+import Cart from './cart.svg';
+import Comment from './comment.svg';
+
+export {Apple, Google, Facebook, Chat, LogoText, Logo, AddressBook, Favorites, Product, Schedules, Settings, Star, VideoLarge, VideoIcon, Cart, Comment}

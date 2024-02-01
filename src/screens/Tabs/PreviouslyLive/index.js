0a0da044a166a4fd41a64f161ae3a299a53b893b
kamrankhan8 <xeroseven@gmail.com>
2024-02-01 16:50:59 +0500

    feat: previously live screen created

---
+++ src/screens/Tabs/PreviouslyLive/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './styles'
+
+export const PreviouslyLive = () => {
+  return (
+    <View>
+      <Text>PreviouslyLive</Text>
+    </View>
+  )
+}

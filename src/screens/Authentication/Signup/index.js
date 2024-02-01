6d1cb5bd0af23f193f90231188da46e6e68f39ed
kamrankhan8 <xeroseven@gmail.com>
2024-02-01 16:45:22 +0500

    feat: signup screen created

---
+++ src/screens/Authentication/Signup/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from './styles'
+
+export const Signup = () => {
+  return (
+    <View>
+      <Text>Signup</Text>
+    </View>
+  )
+}

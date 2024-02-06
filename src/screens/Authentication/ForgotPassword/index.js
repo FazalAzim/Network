d458277c14f64d696827b0dbea9f042942b958aa
kamrankhan8 <xeroseven44@gmail.com>
2024-02-06 16:26:08 +0500

    feat: Header Component imported

--- src/screens/Authentication/ForgotPassword/index.js
+++ src/screens/Authentication/ForgotPassword/index.js
@@ -1,16 +1,24 @@
 import { View, Image } from 'react-native'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { styles } from './styles'
-import { ComponentWrapper, H1, InputWithTitle, MainWrapper, PrimaryButton, RowWrapper, RowWrapperBasic, Seperator, SocialButton, Spacer, Text, TextButton } from '@commons'
+import { ComponentWrapper, H1, HeaderPrimary, InputWithTitle, MainWrapper, PrimaryButton, RowWrapper, RowWrapperBasic, Seperator, SocialButton, Spacer, Text, TextButton } from '@commons'
 import { Logo } from '@react-native/typescript-config'
 import { COLORS, FONTS, IMG, ROUTES } from '@constants'
-import { Apple, Eye, Facebook, Google } from '@assets'
+import { Apple, Back_Caret_Arrow, Eye, Facebook, Google } from '@assets'
 import { height, width } from '@helpers'
 
 
-export const ForgotPassword = () => {
+export const ForgotPassword = ({navigation}) => {
+ 
   return (
    <MainWrapper style={{ backgroundColor: COLORS.WHITE, }}>
+     <HeaderPrimary
+          // title={'Support'}
+          both={true}
+          backArrow={<Back_Caret_Arrow />}
+          backArrowPress ={() => navigation.goBack()}
+          style={{marginTop: 20}}
+        />
     <ComponentWrapper style={{paddingTop: height(10)}}>
       {/* <Image source={IMG.LOGO} style={{width: 167, height: 116, resizeMode:'contain', marginLeft: -35}} /> */}
       <Spacer />
@@ -18,9 +26,10 @@ export const ForgotPassword = () => {
       <Spacer  />
       <Text style={styles.description}>Don't worry! It happens. Please enter the email address linked with your account.</Text>
       <Spacer />
+    
       <InputWithTitle placeholder={'Enter your email'} placeholderTextColor={COLORS.DARK_GRAY}  />
       <Spacer />
-     <PrimaryButton text={'Submit'} onPress={() => {}} style={{width: width(83)}} />
+     <PrimaryButton text={'Submit'} onPress={() => navigation.navigate(ROUTES.RESET_PASSWORD)} style={{width: width(83)}} />
     </ComponentWrapper>
   </MainWrapper>
   )

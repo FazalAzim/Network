fb77f08eea0da158cae953e79f1f3901e8adf4cc
kamrankhan8 <kamran@teknovation.io>
2024-03-04 15:26:58 +0500

    feat: icons exported

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,5 +3,5 @@ export * from './fonts';
 export * from './images';
 export * from './routes';
 export * from './sizes';
-// export * from './icons'
+export * from './icons'
 export * from './dummy';

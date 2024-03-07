4ab79c0eb42f1faab27103d80440ac2765ad5ef7
thenativeguy <xeroseven44@gmail.com>
2024-03-07 15:57:20 +0500

    featA: contact card styles

---
+++ src/commons/H3/styles.js
@@ -0,0 +1,10 @@
+import { COLORS, FONTS } from "@constants";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    text:{
+        fontFamily: FONTS.URBAN_BOLD,
+        fontSize: 18,
+        color: COLORS.BLACK,
+    }
+})

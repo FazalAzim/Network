ed5f70fd0bc1e01035c8cc4be3d0169b4322c9ca
thenativeguy <xeroseven44@gmail.com>
2024-03-07 15:59:58 +0500

    feat: Inbox created

--- src/screens/Tabs/Inbox/index.js
+++ src/screens/Tabs/Inbox/index.js
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { styles } from './styles'
+import { MainWrapper, MessageHeader, SeparatorBasic, Wrapper } from '@commons'
+import { COLORS, users } from '@constants'
+import { height, width } from '@helpers'
+import { ContactCard } from './components'
 
 export const Inbox = () => {
   return (
-    <View>
-      <Text>Inbox</Text>
-    </View>
+    <MainWrapper style={{backgroundColor: COLORS.WHITE}}>
+      <MessageHeader />
+      <Wrapper style={{marginHorizontal: width(5), marginTop: height(2)}}>
+        <SeparatorBasic />
+        <FlatList
+        data={users}
+        renderItem={({item}) => (
+        <>
+          <ContactCard
+          chat={item}
+          />
+          <SeparatorBasic />
+        </>
+        )}
+        />
+      </Wrapper>
+    </MainWrapper>
   )
 }

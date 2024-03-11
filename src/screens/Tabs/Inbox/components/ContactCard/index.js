2feca54bd76595106c818c2207ffed92c2503bab
FazalAzim <fazalazim238@gmail.com>
2024-03-11 17:49:49 +0500

    Contact Card UI

--- src/screens/Tabs/Inbox/components/ContactCard/index.js
+++ src/screens/Tabs/Inbox/components/ContactCard/index.js
@@ -1,12 +1,13 @@
-import { View, Image } from 'react-native'
+import { View, Image, Pressable } from 'react-native'
 import React from 'react'
 import {styles} from './styles'
-import { RowWrapper, Text, Wrapper } from '@commons'
+import { RowWrapper, RowWrapperBasic, Text, Wrapper } from '@commons'
 import { height, width } from '@helpers'
 import moment from 'moment';
 import { COLORS, FONTS } from '@constants'
+import { CheckMarkIcon } from '@assets'
 
-export const ContactCard = ({chat}) => {
+export const ContactCard = ({chat, onPress}) => {
     // console.log(username)
 function formatLastMessageTime(timestamp) {
   const now = moment();
@@ -22,20 +23,26 @@ function formatLastMessageTime(timestamp) {
   }
 }
   return (          
-    <RowWrapper style={{marginHorizontal: width(0), marginVertical: height(1)}}>
+   <Pressable onPress={onPress}>
+     <RowWrapper style={{marginHorizontal: width(0), marginVertical: height(1)}}>
         <Image source={chat.avatar} style={{width: 40, height: 40, borderRadius: 50, resizeMode: 'contain'}} />
         <Wrapper style={{width: width(78), height: height(5), justifyContent:'center',}}>
             <RowWrapper>
-                <Text style={{fontFamily: FONTS.URBAN_BOLD}}>{chat.username}</Text>
+                <RowWrapperBasic style={{alignItems:'center', justifyContent:'center'}}>
+                  <Text style={{fontFamily: FONTS.URBAN_BOLD, marginRight: 4}}>{chat.username}</Text>
+                  <CheckMarkIcon />
+                </RowWrapperBasic>
                 <Text style={{color: COLORS._A3A3}}>{formatLastMessageTime(chat.lastMessageTime)}</Text>
             </RowWrapper>
             <RowWrapper style={{marginTop: 8}}>
                 <Text numOfLines={1} multiline={false} style={{color: COLORS._A3A3}}>{chat.lastMessage}</Text>
-                <Wrapper style={{backgroundColor: COLORS.LIGHT_PINK, width: 18, height: 18, borderRadius: 50, justifyContent: 'center', alignItems: 'center'}}>
+               {chat.unreadMessages !== 0 &&  
+               <Wrapper style={{backgroundColor: COLORS.LIGHT_PINK, width: 18, height: 18, borderRadius: 50, justifyContent: 'center', alignItems: 'center'}}>
                     <Text style={{color: COLORS.PRIMARY_COLOR, fontSize: 11}}>{chat.unreadMessages}</Text>
-                </Wrapper>
+                </Wrapper>}
             </RowWrapper>
         </Wrapper>
     </RowWrapper>
+   </Pressable>
   )
 }

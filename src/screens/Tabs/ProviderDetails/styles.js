819b0d2db68953ffe39516f1fd990819ce662ae0
FazalAzim <fazalazim238@gmail.com>
2024-03-15 18:09:54 +0500

    feature/provider detail screen fix the live comment text

--- src/screens/Tabs/ProviderDetails/styles.js
+++ src/screens/Tabs/ProviderDetails/styles.js
@@ -2,29 +2,34 @@ import { FONTS } from "@constants";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    wrapper: {
-      paddingBottom: 14,
-      marginHorizontal: 18,
-      borderBottomWidth: 1,
-      borderColor: '#C0C0C0'
-    },
-    commentWrapper: {
-      flex: 1,
-      marginTop: 12,
-      flexDirection: 'column'
-    },
-    subscriptionRow: {
-      height: 50,
-      backgroundColor: '#F8F8F8',
-      marginBottom: 10
-    },
-    subscriptionText: {
-      fontSize: 13,
-      fontWeight: '500',
-      lineHeight: 18,
-      fontFamily: FONTS.URBAN_REGULAR
-    },
-    subscribeButtonWrapper: {
-      paddingTop: 8
-    }
-  });
+  wrapper: {
+    paddingBottom: 14,
+    marginHorizontal: 18,
+    borderBottomWidth: 1,
+    borderColor: '#C0C0C0'
+  },
+  commentWrapper: {
+    flex: 1,
+    marginTop: 12,
+    flexDirection: 'column'
+  },
+  headerText: {
+    fontSize: 14,
+    fontWeight: '600',
+    fontFamily: FONTS.URBAN_REGULAR
+  },
+  subscriptionRow: {
+    height: 50,
+    backgroundColor: '#F8F8F8',
+    marginBottom: 10
+  },
+  subscriptionText: {
+    fontSize: 13,
+    fontWeight: '500',
+    lineHeight: 18,
+    fontFamily: FONTS.URBAN_REGULAR
+  },
+  subscribeButtonWrapper: {
+    paddingTop: 12
+  }
+});

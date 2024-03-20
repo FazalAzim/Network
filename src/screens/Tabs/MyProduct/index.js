10ab2f902d9de6744a6a3621de1a2e74d0771983
FazalAzim <fazalazim238@gmail.com>
2024-03-20 13:49:18 +0500

    feature/create tab bar in MyProduct screen

--- src/screens/Tabs/MyProduct/index.js
+++ src/screens/Tabs/MyProduct/index.js
@@ -1,16 +1,43 @@
-import { MainWrapper, RowWrapperBasic, Text } from '@commons'
+import { MainWrapper, RowWrapperBasic, Text, Wrapper } from '@commons'
 import { COLORS, FONTS, ICON } from '@constants'
 import { width } from '@helpers'
-import React from 'react'
+import React, { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+import { ExternalVendor, OwnProduct } from './components'
+import { styles } from './styles'
 
 export const MyProduct = ({ navigation }) => {
+  const [selected, setSelected] = useState(0);
+  const [activeTab, setActiveTab] = useState('Own Product');
+
+  const sections = [
+    { title: 'Own Product', component: <OwnProduct /> },
+    { title: 'External Vendors', component: <ExternalVendor /> },
+  ]
   return (
     <MainWrapper style={{ paddingHorizontal: width(3) }}>
       <RowWrapperBasic style={{ marginVertical: 20 }}>
         <ICON.Entypo name='chevron-thin-left' color={COLORS._3838} size={22} onPress={() => navigation.goBack()} />
         <Text style={{ flex: 1, textAlign: 'center', fontFamily: FONTS.URBAN_BOLD, fontSize: 20, color: COLORS._1E1F }}>Add Products</Text>
       </RowWrapperBasic>
-    </MainWrapper>
+      <Wrapper style={styles.container}>
+        <Wrapper style={styles.buttonContainer}>
+          {sections.map((data, index) => {
+            return (
+              <TouchableOpacity key={index}
+                style={[styles.button, activeTab === data.title ? styles.activeButton : null]}
+                onPress={() => {
+                  setSelected(index);
+                  setActiveTab(data.title)
+                }}>
+                <Text style={[styles.buttonText, activeTab === data.title ? { color: COLORS.PINK_COLOR, fontFamily: FONTS.URBAN_BOLD } : { color: COLORS._1E1F }]}>{data.title}</Text>
+                {activeTab === data.title && <Wrapper style={styles.activeIndicator} />}
+              </TouchableOpacity>
+            )
+          })}
+        </Wrapper>
+      </Wrapper>
+      <Wrapper style={{ marginTop: 14, flex: 1 }}>{sections[selected].component}</Wrapper>
+    </MainWrapper >
   )
 }
-
---
+++ src/screens/Tabs/MyProduct/styles.js
@@ -0,0 +1,31 @@
+import { COLORS, FONTS } from "@constants";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 42,
+    borderBottomWidth: 2,
+    borderColor: COLORS.DARK_GRAY,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    position: 'relative',
+  },
+  activeButton: {
+    borderBottomWidth: 2,
+    borderColor: COLORS.PRIMARY_COLOR,
+  },
+  buttonText: {
+    fontSize: 15,
+    fontFamily: FONTS.URBAN_REGULAR,
+  },
+});

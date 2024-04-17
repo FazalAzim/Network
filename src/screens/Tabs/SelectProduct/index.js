40ea779a81d754618fe452a24fcb2f2a0c044a20
FazalAzim <fazalazim238@gmail.com>
2024-04-17 11:18:41 +0500

    feature/product card design for select product screen

--- src/screens/Tabs/SelectProduct/index.js
+++ src/screens/Tabs/SelectProduct/index.js
@@ -1,21 +1,25 @@
-import { InputWithTitle, MainWrapper, RowWrapperBasic, Text, Wrapper } from '@commons'
-import { COLORS, FONTS, ICON } from '@constants'
+import { InputWithTitle, MainWrapper, PrimaryButton, RowWrapperBasic, Text, Wrapper } from '@commons'
+import { COLORS, FONTS, ICON, ROUTES, product_Cards } from '@constants'
 import { width, height } from '@helpers'
 import React, { useState } from 'react'
-import { TouchableOpacity } from 'react-native'
+import { FlatList, Image} from 'react-native'
 
-const CheckBoxCard = () => {
+const ProductCard = ({item}) => {
   const [isSelected, setSelection] = useState(false);
+
   return (
-    <RowWrapperBasic>
-         {/* <CheckBox
-          value={isSelected}
-          onValueChange={setSelection}
-          // style={styles.checkbox}
-        /> */}
+    <RowWrapperBasic style={{ gap: 14, borderBottomWidth: 1, borderColor: COLORS._B3E8, paddingVertical: height(2), }}>
+      <Text>icon</Text>
+      <Wrapper style={{ justifyContent: 'center', alignItems: 'center', width: 76, height: 76, borderWidth: 1, backgroundColor: '#edf5f2', borderColor: '#b3e8dc', borderRadius: 6, }}>
+        <Image source={item.image} style={{ width: 63, height: 63, resizeMode: 'cover', borderRadius: 6 }} />
+      </Wrapper>
+      <Wrapper style={{ flex: 1 }}>
+        <Text style={{ fontSize: 14, fontFamily: FONTS.URBAN_MEDIUM, color: COLORS._3C3C }}>{item.title}</Text>
+      </Wrapper>
     </RowWrapperBasic>
   )
 }
+
 export const SelectProduct = ({ navigation }) => {
   const [selected, setSelected] = useState(0);
 
@@ -27,9 +31,15 @@ export const SelectProduct = ({ navigation }) => {
           <InputWithTitle placeholder='Search Product' placeholderTextColor={COLORS._C4C4} inputStyle={{ backgroundColor: COLORS._F8F8, borderRadius: 7, height: height(6) }} containerStyle={{ marginBottom: height(0) }} />
         </Wrapper>
       </RowWrapperBasic>
-      <Wrapper>
-        <CheckBoxCard />
+      <Wrapper style={{ paddingHorizontal: width(2) ,flex:1}}>
+        <FlatList
+          data={product_Cards}
+          renderItem={({ item }) => {
+            return <ProductCard item={item}/>;
+          }}
+        />
       </Wrapper>
+      <PrimaryButton text={'Submit'} style={{ borderRadius: 6 }} onPress={() => navigation.navigate(ROUTES.LIVE_PRODUCT)} />
     </MainWrapper >
   )
 }

ded99b3fb6c382beb1c078d8becb18d9ccc02c48
FazalAzim <fazalazim238@gmail.com>
2024-04-23 12:06:14 +0500

    feature/fix sign up screen according to spreadsheet

--- src/commons/Spacer/index.js
+++ src/commons/Spacer/index.js
@@ -10,13 +10,13 @@ const TempSpacer = ({ style, width, height, }) => {
 }
 
 
-export const Spacer = ({ horizontal, height, width, style, isBasic, isSmall, isTiny, isXTiny, isDoubleBase, isStatusBarHeigt, isHeaderHeight, isBottomTabBarHeight }) => {
+export const Spacer = ({ horizontal, height, width, style, isBasic, isSmall, isTiny, isXTiny, isDoubleBase, isStatusBarHeight, isHeaderHeight, isBottomTabBarHeight }) => {
     const tempSize = isXTiny ? sizes.TinyMargin / 2 :
         isTiny ? sizes.TinyMargin :
             isSmall ? sizes.smallMargin :
                 isBasic ? sizes.baseMargin :
                     isDoubleBase ? sizes.doubleBaseMargin :
-                        isStatusBarHeigt ? sizes.statusBarHeight :
+                        isStatusBarHeight ? sizes.statusBarHeight :
                             isHeaderHeight ? sizes.headerHeight :
                                 isBottomTabBarHeight ? sizes.tabBarHeight : sizes.baseMargin
     const defaultHeight = !horizontal ? height || tempSize : 0
--- src/screens/Authentication/Signup/index.js
+++ src/screens/Authentication/Signup/index.js
@@ -1,9 +1,9 @@
 import { View, Text, Image } from 'react-native'
 import React, { useState } from 'react'
 import { styles } from './styles'
-import { ComponentWrapper, H1, InputWithTitle, MainWrapper, PrimaryButton, RowWrapper, RowWrapperBasic, Seperator, SocialButton, Spacer, TextButton } from '@commons'
+import { ComponentWrapper, H1, InputWithTitle, MainWrapper, PrimaryButton, RowWrapper, RowWrapperBasic, Seperator, SocialButton, Spacer, TextButton, Wrapper } from '@commons'
 import { Logo } from '@react-native/typescript-config'
-import { COLORS, FONTS, IMG, ROUTES } from '@constants'
+import { COLORS, FONTS, ICON, IMG, ROUTES } from '@constants'
 import { Apple, Eye, Facebook, Google } from '@assets'
 import { height, width } from '@helpers'
 
@@ -13,28 +13,32 @@ export const Signup = ({navigation}) => {
     setIsPasswordVisible(!isPasswordVisible)
   }
   return (
-     <MainWrapper style={{justifyContent:'center', backgroundColor: COLORS.WHITE}}>
+     <MainWrapper style={{ backgroundColor: COLORS.WHITE}}>
+      <Wrapper style={{marginTop:height(3),marginLeft:width(5)}}>
+        <ICON.Entypo name='chevron-thin-left' color={COLORS._3838} size={20} onPress={() => navigation.goBack()} />
+      </Wrapper>
+      <Spacer isHeaderHeight={true}/>
     <ComponentWrapper>
       {/* <Image source={IMG.LOGO} style={{width: 167, height: 116, resizeMode:'contain', marginLeft: -35}} /> */}
-      <Spacer />
+      {/* <Spacer /> */}
       <H1>Join Skyylytes</H1>
-      <Spacer />
+      <Spacer isDoubleBase={true} />
       <InputWithTitle title={"Name"} placeholder={'John Mandel'} placeholderTextColor={COLORS.DARK_GRAY}  />
       <InputWithTitle title={"Email"} placeholder={'Email'} placeholderTextColor={COLORS.DARK_GRAY}  />
       <InputWithTitle title={"Password"} placeholder={'Password'} secureTextEntry={isPasswordVisible} placeholderTextColor={COLORS.DARK_GRAY} right={<Eye />} onPressRight={onIconPress} />
       <Spacer />
-      <TextButton style={{ alignSelf: 'flex-end' }} text='Forgot Password?' onPress={() => navigation.navigate(ROUTES.FORGOT_PASSWORD)} />
-      <Spacer />
-      <PrimaryButton text={'Signup'} onPress={() => {}} style={{width: width(83)}} />
+      {/* <TextButton style={{ alignSelf: 'flex-end' }} text='Forgot Password?' onPress={() => navigation.navigate(ROUTES.FORGOT_PASSWORD)} />
+      <Spacer /> */}
+      <PrimaryButton text={'Sign Up'} onPress={() => {}} style={{width: width(83)}} />
       <Spacer />
       <Seperator />
-       <RowWrapper>
+       <RowWrapper style={{justifyContent:'center', gap:12}}>
         <SocialButton style={{borderColor: COLORS.BD_COLOR, width: width(20)}} icon={<Facebook />} />
         <SocialButton style={{borderColor: COLORS.BD_COLOR, width: width(20)}} icon={<Google />} />
         <SocialButton style={{borderColor: COLORS.BD_COLOR, width: width(20)}} icon={<Apple />} />
        </RowWrapper>
        <Spacer />
-       <RowWrapperBasic style={{alignSelf: 'center', position:'absolute', bottom: -60}}>
+       <RowWrapperBasic style={{alignSelf: 'center', position:'absolute', bottom: -60, gap:2}}>
           <Text style={{color: COLORS.BLACK, fontFamily: FONTS.URBAN_REGULAR}}>Don't have an account?</Text>
           <TextButton text="Sign In" />
         </RowWrapperBasic>

6ebbdd0be767953fc4c00eed74d21f5d7620dc78
FazalAzim <fazalazim238@gmail.com>
2024-04-23 11:42:11 +0500

    feature/fix sign in screen according to spreadsheet

--- src/screens/Authentication/Login/index.js
+++ src/screens/Authentication/Login/index.js
@@ -24,22 +24,22 @@ export const Login = ({navigation}) => {
       <Image source={IMG.LOGO} style={{width: 167, height: 116, resizeMode:'contain', marginLeft: -35}} />
       <Spacer />
       <H1>Welcome Back!</H1>
-      <Spacer />
+      <Spacer isDoubleBase={true}/>
       <InputWithTitle placeholder={'Email'} placeholderTextColor={COLORS.DARK_GRAY}  />
       <InputWithTitle placeholder={'Password'} secureTextEntry={isPasswordVisible} placeholderTextColor={COLORS.DARK_GRAY} right={<Eye />} onPressRight={onIconPress} />
+      <Spacer isTiny={true} />
+      <TextButton textBtnStyle={{color:COLORS._6A70}} style={{ alignSelf: 'flex-end' }} text='Forgot Password?' onPress={() => navigation.navigate(ROUTES.FORGOT_PASSWORD)} />
       <Spacer />
-      <TextButton style={{ alignSelf: 'flex-end' }} text='Forgot Password?' onPress={() => navigation.navigate(ROUTES.FORGOT_PASSWORD)} />
-      <Spacer />
-      <PrimaryButton text={'Login'} onPress={() => onLoginClick()} style={{width: width(83)}} />
+      <PrimaryButton text={'Sign In'} onPress={() => onLoginClick()} style={{width: width(83)}} />
       <Spacer />
       <Seperator />
-       <RowWrapper>
+       <RowWrapper style={{justifyContent:'center', gap:12}}>
         <SocialButton style={{borderColor: COLORS.BD_COLOR, width: width(20)}} icon={<Facebook />} />
         <SocialButton style={{borderColor: COLORS.BD_COLOR, width: width(20)}} icon={<Google />} />
         <SocialButton style={{borderColor: COLORS.BD_COLOR, width: width(20)}} icon={<Apple />} />
        </RowWrapper>
        <Spacer />
-       <RowWrapperBasic style={{alignSelf: 'center', position:'absolute', bottom: -60}}>
+       <RowWrapperBasic style={{alignSelf: 'center', position:'absolute', bottom: -60, gap:2,}}>
           <Text style={{color: COLORS.BLACK, fontFamily: FONTS.URBAN_REGULAR}}>Don't have an account?</Text>
           <TextButton text="Sign up" onPress={() => navigation.navigate(ROUTES.SIGNUP)} />
         </RowWrapperBasic>

341f7352b30d14eeb73bbe8757a84cd6459a5d9b
FazalAzim <fazalazim238@gmail.com>
2024-05-06 14:11:17 +0500

    feature/Fix video and live screen tabs

--- src/screens/Tabs/LiveProfile/index.js
+++ src/screens/Tabs/LiveProfile/index.js
@@ -1,30 +1,67 @@
-import React from 'react'
-import { MainWrapper, ProfileCard, TabNavbar } from '@commons'
-import { COLORS } from '@constants'
+import React, { useState } from 'react'
+import { HomeCard, MainWrapper, ProductCard, ProfileCard, Text, Wrapper } from '@commons'
+import { COLORS, FONTS, cards, product_Cards } from '@constants'
 import { styles } from './styles'
-import { Products, Videos } from './components'
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view'
+import { FlatList } from 'react-native'
+import { width } from '@helpers'
+
+
+ const Products = () => {
+  return (
+    <Wrapper style={{ flex: 1, flexDirection: 'column', marginTop: 12 }}>
+      <FlatList
+        data={product_Cards}
+        renderItem={({ item }) => {
+          return <ProductCard item={item} />;
+        }}
+      />
+    </Wrapper>
+  )
+}
+
+ const Videos = () => {
+  return (
+    <Wrapper style={{ flex: 1, flexDirection: 'column', marginTop: 12 }}>
+      <FlatList
+        data={cards}
+        renderItem={({ item }) => {
+          return <HomeCard item={item} />;
+        }}
+      />
+    </Wrapper>
+  )
+}
+
+const renderScene = SceneMap({
+  first: Videos,
+  second: Products,
+});
 
 export const LiveProfile = ({ navigation }) => {
-
-  const tabStyles = {
-    focusText: COLORS.BLACK,
-    indicatorStyle: COLORS._3030
-  }
-
+  const [index, setIndex] = useState(0);
   const routes = [
     { key: 'first', title: 'Videos' },
     { key: 'second', title: 'Products' },
   ];
 
-  const tabs = {
-    first: () => <Videos />,
-    second: () => <Products />,
-  };
-
   return (
     <MainWrapper style={{ backgroundColor: COLORS.WHITE }}>
       <ProfileCard navigation={navigation} type={'provider'} />
-      <TabNavbar routes={routes} tabs={tabs} tabStyles={tabStyles} />
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: width(100) }}
+        renderTabBar={props => <TabBar {...props} renderLabel={({ route, color, focused }) => (
+          <Text style={{ color: COLORS.BLACK, fontSize: 15, fontFamily: FONTS.URBAN_REGULAR }}>
+            {route.title}
+          </Text>
+        )} indicatorStyle={{ backgroundColor: COLORS._3030, borderRadius: 16, marginBottom: -1.5, padding: 1, }} style={{
+          backgroundColor: COLORS.WHITE, borderBottomWidth: 1,
+          borderColor: COLORS._D7D7, elevation: 0
+        }} />}
+      />
     </MainWrapper>
   )
 }
--- src/screens/Tabs/VideoScreen/index.js
+++ src/screens/Tabs/VideoScreen/index.js
@@ -1,30 +1,70 @@
-import React from 'react'
-import { MainWrapper, VideoCard, TabNavbar, ScrollWrapper } from '@commons'
-import { COLORS } from '@constants'
-import { LiveComments, Products } from './components'
+import React, { useState } from 'react'
+import { MainWrapper, VideoCard, ScrollWrapper, Wrapper, ProductCard, CommentCard, Text, InputWithTitle } from '@commons'
+import { COLORS, FONTS, comment_Cards, product_Cards } from '@constants'
+import { FlatList } from 'react-native'
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view'
+import { width } from '@helpers'
+import { Plane } from '@assets'
+
+const Products = () => {
+  return (
+    <Wrapper style={{ flex: 1, flexDirection: 'column', marginTop: 12 }}>
+      <FlatList
+        data={product_Cards}
+        renderItem={({ item }) => {
+          return <ProductCard item={item} onClick={() => navigation.navigate(ROUTES.PRODUCT_DETAIL)} />;
+        }}
+      />
+    </Wrapper>
+  )
+}
+
+const LiveComments = () => {
+  return (
+    <Wrapper style={{ flex: 1, flexDirection: 'column', marginTop: 12 }}>
+      <FlatList
+        data={comment_Cards}
+        renderItem={({ item }) => {
+          return <CommentCard item={item} />;
+        }}
+      />
+      {/* <InputWithTitle placeholder={'Type your comment'} placeholderTextColor={COLORS.DARK_GRAY} right={<Plane />} /> */}
+    </Wrapper>
+  )
+}
+
+const renderScene = SceneMap({
+  first: Products,
+  second: LiveComments,
+});
 
 export const VideoScreen = ({ route, navigation }) => {
   const { item } = route?.params;
-  const tabStyles = {
-    focusText: COLORS.BLACK,
-    indicatorStyle: COLORS._3030
-  }
+  const [index, setIndex] = useState(0);
 
   const routes = [
     { key: 'first', title: 'Products' },
     { key: 'second', title: 'Live Comments' },
   ];
 
-  const tabs = {
-    first: () => <Products navigation={navigation} />,
-    second: () => <LiveComments />,
-  };
-
   return (
-    <MainWrapper style={{flex:1, backgroundColor: COLORS.WHITE }}>
+    <MainWrapper style={{ flex: 1, backgroundColor: COLORS.WHITE }}>
       {/* <ScrollWrapper> */}
-        <VideoCard onClick={() => navigation.goBack()} paramData={item} navigation={navigation} services={false} />
-        <TabNavbar routes={routes} tabs={tabs} tabStyles={tabStyles} />
+      <VideoCard onClick={() => navigation.goBack()} paramData={item} navigation={navigation} services={false} />
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: width(100) }}
+        renderTabBar={props => <TabBar {...props} renderLabel={({ route, color, focused }) => (
+          <Text style={{ color: COLORS.BLACK, fontSize: 15, fontFamily: FONTS.URBAN_REGULAR }}>
+            {route.title}
+          </Text>
+        )} indicatorStyle={{ backgroundColor: COLORS._3030, borderRadius: 16, marginBottom: -1.5, padding: 1, }} style={{
+          backgroundColor: COLORS.WHITE, borderBottomWidth: 1,
+          borderColor: COLORS._D7D7, elevation: 0
+        }} />}
+      />
       {/* </ScrollWrapper> */}
     </MainWrapper>
   )
